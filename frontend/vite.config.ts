8a1acef162784684a7f38356fcf484350054e794
jpucic00 <jpucic00@gmail.com>
2025-06-11 10:07:51 +0200

    Production configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,16 +4,27 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: '../backend/dist/public'
-  },
   server: {
+    port: 5173,
     proxy: {
       '/api': {
-        target: process.env.NODE_ENV === 'production' 
-          ? 'https://sponsorship-app-production.up.railway.app/' 
+       target: process.env.NODE_ENV === 'production' 
+          ? 'http://localhost:3001' 
           : 'http://localhost:3001',
-        changeOrigin: true
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', proxyReq, req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
       }
     }
   }
